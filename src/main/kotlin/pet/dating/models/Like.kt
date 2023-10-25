6490fe1f298f82336824341cb3b9a8d04b6b8d10
darg0ltz <xfoj@bk.ru>
2023-10-25 20:28:09 +0300

    fix: исправить импорты

--- src/main/kotlin/pet/dating/models/Like.kt
+++ src/main/kotlin/pet/dating/models/Like.kt
@@ -11,12 +11,12 @@ open class Like {
     @MapsId("userId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
-    open var user: pet.dating.models.User? = null
+    open var user: User? = null
 
     @MapsId("likedUserId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "liked_user_id", nullable = false)
-    open var likedUser: pet.dating.models.User? = null
+    open var likedUser: User? = null
 
     @Column(name = "is_match")
     open var isMatch: Boolean? = null
