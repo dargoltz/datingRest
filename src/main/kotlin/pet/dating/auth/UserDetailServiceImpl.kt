f087ed044ced7bdc6620b1ac63645d07aa6be039
darg0ltz <xfoj@bk.ru>
2023-10-30 23:51:25 +0300

    feat(auth): создать userDetailsService для user

---
+++ src/main/kotlin/pet/dating/auth/UserDetailServiceImpl.kt
@@ -0,0 +1,22 @@
+package pet.dating.auth
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import pet.dating.repositories.UserRepository
+
+@Service
+class UserDetailsServiceImpl : UserDetailsService {
+
+    @Autowired
+    private val userRepository: UserRepository? = null
+
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository?.findByUsername(username)
+            ?: throw UsernameNotFoundException("User not found")
+        return MyUserDetails(user)
+    }
+}
