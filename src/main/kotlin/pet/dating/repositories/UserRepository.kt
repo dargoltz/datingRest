d05ca7d20107a054bca18b5ab2f384fe1a2162ca
darg0ltz <xfoj@bk.ru>
2023-10-30 23:48:59 +0300

    feat(data): создать репозиторий для user

---
+++ src/main/kotlin/pet/dating/repositories/UserRepository.kt
@@ -0,0 +1,10 @@
+package pet.dating.repositories;
+
+import org.springframework.data.repository.CrudRepository
+import pet.dating.models.User
+
+interface UserRepository : CrudRepository<User, Int> {
+
+    fun findByUsername(username: String): User?
+
+}
