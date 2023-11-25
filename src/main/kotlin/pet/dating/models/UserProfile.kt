b0ee8a33cd9e12215822762aee1e5a8323fbf874
darg0ltz <xfoj@bk.ru>
2023-11-25 03:50:42 +0300

    feat(controller): add mapper for transform entity to dto

--- src/main/kotlin/pet/dating/models/UserProfile.kt
+++ src/main/kotlin/pet/dating/models/UserProfile.kt
@@ -1,17 +1,34 @@
 package pet.dating.models
 
 import jakarta.persistence.*
+import pet.dating.dto.UserProfileDto
 
 @Entity
 @Table(name = "user_profile")
 open class UserProfile {
     @Id
     @Column(name = "\"user\"", nullable = false)
-    open var user: String? = null
+    open var username: String? = null
 
     @Column(name = "info", nullable = false)
     open var info: String? = null
 
     @Column(name = "contacts", nullable = false)
     open var contacts: String? = null
+
+    fun toUserProfileDto(showContacts: Boolean): UserProfileDto {
+        return if(showContacts) {
+            UserProfileDto(
+                username = username!!,
+                info = info!!,
+                contacts = contacts
+            )
+        } else {
+            UserProfileDto(
+                username = username!!,
+                info = info!!,
+                contacts = "no access"
+            )
+        }
+    }
 }
