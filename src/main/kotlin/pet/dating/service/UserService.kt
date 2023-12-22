6853cd4b69f685f6714cc023a873a81bb4158cea
darg0ltz <xfoj@bk.ru>
2023-12-22 16:53:12 +0300

    feat: remove moved methods

--- src/main/kotlin/pet/dating/service/UserService.kt
+++ src/main/kotlin/pet/dating/service/UserService.kt
@@ -28,43 +28,6 @@ class UserService(
         }
     }
 
-    private fun getUserProfile(username: String, isMatch: Boolean): UserProfileDto {
-
-        val user = userProfileRepository.findById(username).orElseThrow()
-
-        return if (isMatch) {
-            user.toUserProfileDto(true)
-        } else {
-            user.toUserProfileDto(false)
-        }
-
-    }
-
-    fun getMatchedUsers(user: String): List<UserProfileDto> {
-
-        val likes = likeRepository.findAll()
-
-        val thoseILiked = likes.filter { it.id?.user == user }.map { it.id?.likedUser }
-        val whoLikeMe = likes.filter { it.id?.likedUser == user }.map { it.id?.user }
-
-        val matches = thoseILiked.intersect(whoLikeMe.toSet()).filterNotNull()
-
-        return matches.map { getUserProfile(it, true) }
-    }
-
-    fun getUnlikedUsers(user: String): List<UserProfileDto> {
-
-        val likes = likeRepository.findAll()
-        val users = userProfileRepository.findAll()
-
-        val thoseILiked =
-            likes.filter { it.id?.user == user }.mapNotNull { it.id?.likedUser }
-        val unlikedUsernames =
-            users.filter { it.username != user && it.username !in thoseILiked }.mapNotNull { it.username }
-
-        return unlikedUsernames.map { getUserProfile(it, false) }
-    }
-
     fun like(user: String, likedUser: String): String {
 
         if (user == likedUser) {
