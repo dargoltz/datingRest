084c040eb57bb368a7cfce6002490e09e2f6c4cb
darg0ltz <xfoj@bk.ru>
2023-12-25 00:08:00 +0300

    feat: changed with processing decomposition

--- src/main/kotlin/pet/dating/service/LikeService.kt
+++ src/main/kotlin/pet/dating/service/LikeService.kt
@@ -1,10 +1,13 @@
 package pet.dating.service
 
 import org.springframework.stereotype.Service
+import pet.dating.dto.ProcessingResult
+import pet.dating.enums.LikeAction
 import pet.dating.models.Like
 import pet.dating.models.LikeId
 import pet.dating.repositories.LikeRepository
 import pet.dating.repositories.UserProfileRepository
+import java.util.*
 
 @Service
 class LikeService(
@@ -12,47 +15,64 @@ class LikeService(
     private val likeRepository: LikeRepository
 ) {
 
-    fun like(user: String, likedUser: String): String {
+    fun processLike(user: String, processingUser: String, action: LikeAction): ProcessingResult {
+        val validationResult = validateBefore(user, processingUser)
 
-        if (!validateBeforeLike(user, likedUser)) return "something wrong"
-
-        likeRepository.save(createLike(user, likedUser))
-
-        return "user $likedUser was liked"
-    }
-
-    fun removeLike(user: String, dislikedUser: String): String {
-
-        val like = createLike(user, dislikedUser)
-        likeRepository.delete(like)
-
-        return "removed like from user $dislikedUser"
-    }
-
-    private fun validateBeforeLike(user: String, likedUser: String): Boolean {
-        if (user == likedUser) {
-            return false
+        if (validationResult != null) {
+            return validationResult
         }
 
-        if (!isExistingUser(likedUser)) {
-            return false
+        val createdLike = createEntityLike(user, processingUser)
+        val foundLike = likeRepository.findById(createdLike.id!!)
+
+        return when (action) {
+            LikeAction.LIKE -> like(createdLike, foundLike)
+            LikeAction.DISLIKE -> dislike(createdLike, foundLike)
         }
 
-        return !alreadyLiked(user, likedUser)
+    }
+
+    private fun validateBefore(user: String, processingUser: String): ProcessingResult? {
+        if (user == processingUser) {
+            return ProcessingResult("can't like/dislike yourself", 400)
+        }
+
+        if (processingUser.isBlank()) {
+            return ProcessingResult("empty username", 400)
+        }
+
+        if (isExistingUser(processingUser)) {
+            return ProcessingResult("user not found", 400)
+        }
+
+        return null
+    }
+
+    private fun like(createdLike: Like, foundLike: Optional<Like>): ProcessingResult {
+        if (foundLike.isPresent) {
+            return ProcessingResult("already liked", 400)
+        }
+
+        likeRepository.save(createdLike)
+
+        return ProcessingResult("user ${createdLike.id?.likedUser} was liked", 200)
+    }
+
+    private fun dislike(createdLike: Like, foundLike: Optional<Like>): ProcessingResult {
+        if (!foundLike.isPresent) {
+            return ProcessingResult("user not liked", 400)
+        }
+
+        likeRepository.delete(createdLike)
+
+        return ProcessingResult("user ${createdLike.id?.likedUser} was disliked", 200)
     }
 
     private fun isExistingUser(username: String): Boolean {
-
         return userProfileRepository.findById(username).isPresent
     }
 
-    private fun alreadyLiked(user: String, likedUser: String): Boolean {
-
-        return likeRepository.findById(createLike(user, likedUser).id!!).isPresent
-    }
-
-    private fun createLike(user: String, likedUser: String): Like {
-
+    private fun createEntityLike(user: String, likedUser: String): Like {
         return Like().getLike(LikeId().getLikeId(user, likedUser))
     }
 
