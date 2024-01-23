78047fc2af9016af329fdf189ead8c0fb7dbb473
darg0ltz <xfoj@bk.ru>
2024-01-23 17:41:06 +0300

    feat: implement bcrypt lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.liquibase:liquibase-core")
 	implementation("org.postgresql:postgresql:42.6.0")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("org.mindrot:jbcrypt:0.4")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework:spring-webflux")
