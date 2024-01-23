f94b7853993517fb49f0b0a804c4bd65f86edc2e
darg0ltz <xfoj@bk.ru>
2024-01-23 18:05:23 +0300

    feat: jwt signature moved to application.yaml

--- src/main/kotlin/pet/dating/service/JWTService.kt
+++ src/main/kotlin/pet/dating/service/JWTService.kt
@@ -2,25 +2,30 @@ package pet.dating.service
 
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class JWTService {
 
-    val secretKey = "yourSecretKey"
+    @Value("\${keys.jwt_signature}")
+    private val signature: String? = null
 
     fun createToken(username: String): String {
         return Jwts.builder()
             .setSubject(username)
             .setExpiration(Date())
-            .signWith(SignatureAlgorithm.HS512, secretKey)
+            .signWith(SignatureAlgorithm.HS512, signature)
             .compact()
     }
 
     fun parseTokenAndGetUsername(token: String): String? {
         return try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).body
+            Jwts.parser()
+                .setSigningKey(signature)
+                .parseClaimsJws(token)
+                .body
         } catch (e: Exception) {
             null
         }?.subject
