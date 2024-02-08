5d5d29742f7f850286e98bfcab214b1f4a0fcef3
darg0ltz <xfoj@bk.ru>
2024-02-08 18:04:45 +0300

    test: add autoconfigure mock mvc annotation

--- src/test/kotlin/pet/dating/service/integration/JWTAuthTest.kt
+++ src/test/kotlin/pet/dating/service/integration/JWTAuthTest.kt
@@ -2,6 +2,7 @@ package pet.dating.service.integration
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JWTAuthTest {
 
     @Autowired
